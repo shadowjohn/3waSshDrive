31d529100d457a8dcbbefb39e0c41a3d70b34812
羽山 <linainverseshadow@gmail.com>
2026-09-18 18:58:58 +0800

    test: separate WinFsp loader metadata contracts

--- tests/3waSshDrive.FileSystem.Tests/WinFspAssemblyMetadataTests.cs
+++ tests/3waSshDrive.FileSystem.Tests/WinFspAssemblyMetadataTests.cs
@@ -9,14 +9,21 @@ namespace ThreeWa.SshDrive.FileSystem.Tests
     public sealed class WinFspAssemblyMetadataTests
     {
         [TestMethod]
-        public void CompiledWinFspAssembly_MatchesNativeLoaderContract()
+        public void CompiledWinFspAssembly_UsesNativeProductName()
         {
             var assembly = typeof(FileSystemBase).Assembly;
             var product = assembly.GetCustomAttribute<AssemblyProductAttribute>();
-            var fileVersion = FileVersionInfo.GetVersionInfo(assembly.Location);
 
             Assert.IsNotNull(product);
             Assert.AreEqual("WinFsp", product.Product);
+        }
+
+        [TestMethod]
+        public void CompiledWinFspAssembly_UsesCompatibleFileVersion()
+        {
+            var assembly = typeof(FileSystemBase).Assembly;
+            var fileVersion = FileVersionInfo.GetVersionInfo(assembly.Location);
+
             Assert.AreEqual(2, fileVersion.FileMajorPart);
             Assert.AreEqual(1, fileVersion.FileMinorPart);
         }
