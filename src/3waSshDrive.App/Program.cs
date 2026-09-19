ec468a0ac0b2cfd62e584797d46cd4a6403535f9
羽山 <linainverseshadow@gmail.com>
2026-09-19 13:00:30 +0800

    crash 時有 log 佐證

--- src/3waSshDrive.App/Program.cs
+++ src/3waSshDrive.App/Program.cs
@@ -1,5 +1,7 @@
 using System;
+using System.Threading.Tasks;
 using System.Windows.Forms;
+using ThreeWa.SshDrive.Core.Logging;
 
 namespace ThreeWa.SshDrive.App
 {
@@ -8,9 +10,51 @@ namespace ThreeWa.SshDrive.App
         [STAThread]
         private static void Main()
         {
-            Application.EnableVisualStyles();
-            Application.SetCompatibleTextRenderingDefault(false);
-            Application.Run(new MainForm());
+            // ponytail: Hook all unhandled exception sinks to CrashLogger for automatic crash recording.
+            Application.SetUnhandledExceptionMode(UnhandledExceptionMode.CatchException);
+
+            // 1. UI Thread unhandled exceptions
+            Application.ThreadException += (sender, e) =>
+            {
+                CrashLogger.Log("Application.ThreadException", e.Exception);
+                try
+                {
+                    MessageBox.Show(
+                        $"程式發生未預期的異常 (已記錄至 log 目錄)：\n\n{e.Exception.Message}",
+                        "3waSshDrive - 異常錯誤",
+                        MessageBoxButtons.OK,
+                        MessageBoxIcon.Error);
+                }
+                catch
+                {
+                }
+            };
+
+            // 2. Non-UI / background thread unhandled exceptions
+            AppDomain.CurrentDomain.UnhandledException += (sender, e) =>
+            {
+                var ex = e.ExceptionObject as Exception ?? new Exception(e.ExceptionObject?.ToString());
+                CrashLogger.Log($"AppDomain.UnhandledException (IsTerminating={e.IsTerminating})", ex);
+            };
+
+            // 3. Unobserved task exceptions
+            TaskScheduler.UnobservedTaskException += (sender, e) =>
+            {
+                CrashLogger.Log("TaskScheduler.UnobservedTaskException", e.Exception);
+                e.SetObserved();
+            };
+
+            try
+            {
+                Application.EnableVisualStyles();
+                Application.SetCompatibleTextRenderingDefault(false);
+                Application.Run(new MainForm());
+            }
+            catch (Exception ex)
+            {
+                CrashLogger.Log("Program.Main", ex);
+                throw;
+            }
         }
     }
 }
